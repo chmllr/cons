5eac60b930defaf948603c18711fc4639ee1eaa5
Christian Müller <chrmllr@gmail.com>
2019-01-02 15:09:17 +0100

    reads time from testlib photos

--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module github.com/chmllr/imgtb
+
+require github.com/rwcarlsen/goexif v0.0.0-20180518182100-8d986c03457a
--- main.go
+++ main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/rwcarlsen/goexif/exif"
+	"github.com/rwcarlsen/goexif/mknote"
+)
 
 func main() {
-	fmt.Println("imgtb")
+	path := "./testlib"
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range files {
+		t, err := datetime(filepath.Join(path, f.Name()))
+		if err != nil {
+			fmt.Printf("skipping file %s due to errors: %v", f.Name, err)
+			continue
+		}
+		fmt.Println(f.Name(), t)
+	}
+}
+
+func datetime(path string) (time.Time, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return time.Now(), err
+	}
+
+	exif.RegisterParsers(mknote.All...)
+
+	x, err := exif.Decode(f)
+	if err != nil {
+		return time.Now(), err
+	}
+
+	return x.DateTime()
 }
