b88847e9fe0a951b836d906d9f22abd830b94da3
Christian Müller <chrmllr@gmail.com>
2019-01-02 20:39:03 +0100

    implements moving into folders

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -13,20 +12,43 @@ import (
 )
 
 func main() {
-	path := "./testlib"
-	files, err := ioutil.ReadDir(path)
+	sourceFolder := "./import"
+	files, err := ioutil.ReadDir(sourceFolder)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	log.Println("files found:", len(files))
+	folders := map[string][]string{}
 	for _, f := range files {
-		t, err := datetime(filepath.Join(path, f.Name()))
+		t, err := datetime(filepath.Join(sourceFolder, f.Name()))
 		if err != nil {
-			fmt.Printf("skipping file %s due to errors: %v", f.Name, err)
+			log.Printf("skipping file %s due to errors: %v\n", f.Name(), err)
 			continue
 		}
-		fmt.Println(f.Name(), t)
+		folder := t.Format("2006/01/02")
+		folders[folder] = append(folders[folder], f.Name())
 	}
+	log.Println("new folders required:", len(folders))
+	libFolder := "./testlib"
+	imported := 0
+	for folder, files := range folders {
+		destinationPath := filepath.Join(libFolder, folder)
+		if err := os.MkdirAll(destinationPath, os.ModePerm); err != nil && !os.IsExist(err) {
+			log.Printf("couldn't create folder %q: %v\n", destinationPath, err)
+			continue
+		}
+		for _, fileName := range files {
+			from := filepath.Join(sourceFolder, fileName)
+			to := filepath.Join(destinationPath, fileName)
+			err := os.Rename(from, to)
+			if err != nil {
+				log.Printf("couldn't move file from %q to %q: %v\n", from, to, err)
+			}
+			imported++
+		}
+	}
+	log.Printf("files succesfully imported: %d/%d", imported, len(files))
 }
 
 func datetime(path string) (time.Time, error) {
