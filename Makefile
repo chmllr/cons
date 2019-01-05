c11a24aed248c629a9722e80fa390e7b2aa1021d
Christian Müller <chrmllr@gmail.com>
2019-01-05 14:37:39 +0100

    implements a command interface

--- Makefile
+++ Makefile
@@ -2,7 +2,14 @@ all:
 	go run main.go
 
 clean-testlib:
-	rm -rf testlib/*
+	rm -rf testdata/lib/*
 
 restore-import:
-	cp -rf import-original/* import/
+	cp -rf testdata/import-original/* testdata/import/
+
+restore:
+	make clean-testlib
+	make restore-import
+
+show:
+	tree testdata/lib
---
+++ imp/import.go
@@ -0,0 +1,100 @@
+package imp
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/rwcarlsen/goexif/exif"
+	"github.com/rwcarlsen/goexif/mknote"
+)
+
+func Import(libFolder, sourceFolder string) {
+	files, err := ioutil.ReadDir(sourceFolder)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("files found:", len(files))
+	folders := map[string][]string{}
+	for _, f := range files {
+		t, err := datetime(filepath.Join(sourceFolder, f.Name()))
+		if err != nil {
+			log.Printf("skipping file %s due to errors: %v\n", f.Name(), err)
+			continue
+		}
+		folder := t.Format("2006/01/02")
+		folders[folder] = append(folders[folder], f.Name())
+	}
+	log.Println("new folders required:", len(folders))
+	imported := 0
+	for folder, files := range folders {
+		destinationPath := filepath.Join(libFolder, folder)
+		if err := os.MkdirAll(destinationPath, os.ModePerm); err != nil && !os.IsExist(err) {
+			log.Printf("couldn't create folder %q: %v\n", destinationPath, err)
+			continue
+		}
+		for _, fileName := range files {
+			from := filepath.Join(sourceFolder, fileName)
+			to := filepath.Join(destinationPath, fileName)
+			err := moveFile(from, to)
+			if err != nil {
+				log.Printf("couldn't move file from %q to %q: %v\n", from, to, err)
+				continue
+			}
+			imported++
+		}
+	}
+	log.Printf("files succesfully imported: %d/%d", imported, len(files))
+}
+
+func datetime(path string) (time.Time, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return time.Now(), err
+	}
+
+	exif.RegisterParsers(mknote.All...)
+
+	x, err := exif.Decode(f)
+	if err != nil {
+		return time.Now(), err
+	}
+
+	return x.DateTime()
+}
+
+func moveFile(from, to string) error {
+	stat, err := os.Stat(to)
+	if err != nil && !os.IsNotExist(err) {
+		return err
+	}
+	destinationNonEmpty := stat != nil
+
+	data, err := ioutil.ReadFile(from)
+	if err != nil {
+		return err
+	}
+
+	if destinationNonEmpty {
+		destData, err := ioutil.ReadFile(to)
+		if err != nil {
+			return fmt.Errorf("file %q exists and couldn't be read: %v", to, err)
+		}
+		for i := range destData {
+			if i >= len(data) || data[i] != destData[i] {
+				return fmt.Errorf("file %q already exists and is different than file %q", to, from)
+			}
+		}
+		return fmt.Errorf("file %q is already imported", from)
+	}
+
+	if err := ioutil.WriteFile(to, data, 0644); err != nil {
+		return err
+	}
+
+	return os.Remove(from)
+}
